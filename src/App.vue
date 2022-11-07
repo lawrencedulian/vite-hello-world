1ad50c2e256b772ef9250f1b619076184c4d86d1
lawrencedulian <lawrenceedulian@gmail.com>
2022-11-07 17:52:21 +0100

    Bonus Done

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,6 @@ export default {
 }
 </script>
 
-
 <template>
   <AppTitleVue></AppTitleVue>
   <AppHeroVue></AppHeroVue>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data() {
+        return {
+            image: "https://picsum.photos/seed/picsum/400/400"
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="jumbotron">
+        <img :src="image" alt="">
+    </div>
+</template>
+
+<style scoped>
+.jumbotron {
+    width: 100%;
+    height: 400px;
+}
+
+.jumbotron img {
+    width: 100%;
+    height: 100%;
+
+    object-fit: cover;
+}
+</style>
