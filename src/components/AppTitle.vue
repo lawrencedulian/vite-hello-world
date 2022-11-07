0c1c82fc5a4b78ed280778eafcfaa10245d24f7f
lawrencedulian <lawrenceedulian@gmail.com>
2022-11-07 17:15:01 +0100

    first App

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+            title: "La mia prima app con Vite!"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1> {{title}}</h1>
+</template>
+
+<style scoped></style>
